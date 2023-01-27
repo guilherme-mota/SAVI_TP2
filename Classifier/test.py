4a01f2ed7bee54a701367332033dc5f41457a33d
LuisPires3105 <114987371+LuisPires3105@users.noreply.github.com>
2023-01-27 21:55:05 +0000

    Add files via upload

--- Classifier/test.py
+++ Classifier/test.py
@@ -7,6 +7,10 @@
 import glob
 import torch
 import random
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sn
+import pandas as pd
 from tqdm import tqdm
 from model import Model
 from dataset import Dataset
@@ -14,6 +18,7 @@ from statistics import mean
 from colorama import Fore, Style
 from data_visualizer import DataVisualizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,classification_report
 from classification_visualizer import ClassificationVisualizer
 
 
@@ -39,11 +44,11 @@ def main():
     # -----------------------------------------------------------------
     # Datasets
     # -----------------------------------------------------------------
-    dataset_path = '/home/guilherme/SAVI_Datasets/rgbd-dataset/test'
+    dataset_path = '../docs/rgbd-dataset/test'
     image_filenames = glob.glob(dataset_path + '/*.png')
 
     # Sample only a few images to speed up testing
-    image_filenames = random.sample(image_filenames, k=400)
+    image_filenames = random.sample(image_filenames, k=1536)
 
     # Create the dataset
     dataset_test = Dataset(image_filenames)
@@ -54,6 +59,8 @@ def main():
 
     # Run test in batches ---------------------------------------
     test_losses = []
+    labels = []
+    predicted_labels = []
     idx = 0
     for batch_idx, (image_t, label_t) in tqdm(enumerate(loader_test), total=len(loader_test), desc=Fore.GREEN + 'Testing batches' + str(idx) +  Style.RESET_ALL):
 
@@ -70,8 +77,25 @@ def main():
 
         test_visualizer.draw(image_t, label_t, label_t_predicted)
 
+        
+        for probability in label_t_predicted.cpu().tolist():
+            predicted_labels.append(np.argmax(probability))
+        labels.append(label_t.cpu().tolist())
+
         # Update Index
         idx = batch_idx + 1
+    print(test_losses)
+    labels = [item for sublist in labels for item in sublist]
+    total_matrix_of_confusion = confusion_matrix(labels, predicted_labels)
+    print(classification_report(labels, predicted_labels))
+    print("Number of Labels: ",len(labels),"Number of Predicted Labels: ",len(predicted_labels))
+    df_cm = pd.DataFrame(total_matrix_of_confusion)
+    plt.figure()
+    sn.set(font_scale=0.7) # for label size
+    sn.heatmap(df_cm, annot=True)
+    plt.show()
+
+
     # --------------------------------------------------------
 
 if __name__ == "__main__":
